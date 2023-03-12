84574cf896cd44d036124eec31ae815792151c69
ZOUHEIRBN <44086309+ZOUHEIRBN@users.noreply.github.com>
2023-03-12 01:29:00 +0100

    Update gen.py

--- src/gen.py
+++ src/gen.py
@@ -1,19 +1,23 @@
 import os, sys
-from utils import *
-from configuration import Configuration
+from .utils import *
+from .configuration import Configuration
 
 def generate_(src, dest):
+    
     writer = codecs.open(os.path.abspath(dest), 'w', encoding='utf-8')
-    for i, sen in enumerate(read_conll(src)):
-        if is_projective([e.head for e in sen[1:]]):
-            conf = Configuration(sen)
-            while not conf.is_terminal_state():
-                act, l = conf.next_gold_action()
-                label = (act + ':' + l) if l else act
-                wf, pf,lf = conf.features()
-                writer.write(' '.join(wf) + ' ' + ' '.join(pf)  + ' ' + ' '.join(lf)+ ' ' + label + '\n')
-                conf.do(act, l)
-        if (i+1) % 100 == 0: sys.stdout.write(str(i+1) + '...')
+    conll_data = read_conll(src)
+    with tqdm.tqdm(total=len(conll_data), position=0, leave=True) as pbar:
+        for i, sen in enumerate(conll_data):
+            if is_projective([e.head for e in sen[1:]]):
+                conf = Configuration(sen)
+                while not conf.is_terminal_state():
+                    act, l = conf.next_gold_action()
+                    label = (act + ':' + l) if l else act
+                    wf, pf,lf = conf.features()
+                    writer.write(' '.join(wf) + ' ' + ' '.join(pf)  + ' ' + ' '.join(lf)+ ' ' + label + '\n')
+                    conf.do(act, l)
+            #if (i+1) % 100 == 0: sys.stdout.write(str(i+1) + '...')
+	    pbar.update(1)
     writer.close()
     print('done')
 

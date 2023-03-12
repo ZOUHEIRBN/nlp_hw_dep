98038e039fdc2bb3a2ef4a234444ae534ff18066
ZOUHEIRBN <44086309+ZOUHEIRBN@users.noreply.github.com>
2023-03-12 01:20:34 +0100

    Update gen.py

--- src/gen.py
+++ src/gen.py
@@ -2,16 +2,23 @@ import os, sys
 from utils import *
 from configuration import Configuration
 
-writer = codecs.open(os.path.abspath(sys.argv[2]), 'w', encoding='utf-8')
-for i, sen in enumerate(read_conll(os.path.abspath(sys.argv[1]))):
-    if is_projective([e.head for e in sen[1:]]):
-        conf = Configuration(sen)
-        while not conf.is_terminal_state():
-            act, l = conf.next_gold_action()
-            label = (act + ':' + l) if l else act
-            wf, pf,lf = conf.features()
-            writer.write(' '.join(wf) + ' ' + ' '.join(pf)  + ' ' + ' '.join(lf)+ ' ' + label + '\n')
-            conf.do(act, l)
-    if (i+1) % 100 == 0: sys.stdout.write(str(i+1) + '...')
-writer.close()
-print('done')
+def generate_(src, dest):
+    writer = codecs.open(os.path.abspath(dest), 'w', encoding='utf-8')
+    for i, sen in enumerate(read_conll(src)):
+        if is_projective([e.head for e in sen[1:]]):
+            conf = Configuration(sen)
+            while not conf.is_terminal_state():
+                act, l = conf.next_gold_action()
+                label = (act + ':' + l) if l else act
+                wf, pf,lf = conf.features()
+                writer.write(' '.join(wf) + ' ' + ' '.join(pf)  + ' ' + ' '.join(lf)+ ' ' + label + '\n')
+                conf.do(act, l)
+        if (i+1) % 100 == 0: sys.stdout.write(str(i+1) + '...')
+    writer.close()
+    print('done')
+
+
+if __name__ == "__main__":
+	src = sys.argv[1]
+	dest = sys.argv[2]
+	generate_(src, dest)
